74b4931e4a9cb7ef9cc0c292086869d6fb6907aa
zacwang <knde2019@gmail.com>
2025-05-15 23:23:42 +1000

    update exmaples.py with more details

--- examples.py
+++ examples.py
@@ -1,9 +1,14 @@
 from global_temperature.tools.download import download
 
-# The available range of years is from 1990 to 2025
-start_year = 2023
-end_year = 2024
+# 1. Download the data. The available range of years is from 1990 to 2025.
+# This is not an API service, so please don't abuse the API. If possible, please download the data only once.
+
+# specify a path where you want to download the data. An absolute path is recommended.
 target_path = "examples/data"
+
+# Method1: download the data for a specific year range (both inclusive)
+# start_year = 2023
+# end_year = 2024
 
 # the function will download the data from the API and returnes the years that failed to download
 # failed_years = download(
@@ -13,26 +18,49 @@ target_path = "examples/data"
 #     # delete_archived_files=False,  # if you want to keep the zipped files
 # )
 
-# Or can you specify the years that you want to download
-years = [2021, 2022, 2023, 2025]
-# failed_years = download(years=years, target_path=target_path)
+# Method2: you can specify the years that you want to download
+years = [2021, 2022, 2025]
+failed_years = download(years=years, target_path=target_path)
 
-# 2. After downloading the file, you can query any locations
+
+# 2. After downloading the file, you can query any locations globally (only land areas are supported)
+# The tool will automatically find the nearest grid point to the specified location and return the temperature data.
 import global_temperature as gt
 
 temperature_monthly = gt.TemperatureFactory.create_temperature_object(
     data_type="monthly",
     # specify the folder that you downloaded the data (if possible, use abosolute path)
     source_folder=target_path,
+    # specify search radius in degrees, the default is 0.1 degrees
+    # this means the tool will find the nearest grid point within 0.1 degrees. You can increase the value to get a larger search radius.
+    search_radius=0.1,
 )
 
 # Query the temperature for a specific year, month, latitude, and longitude
 year = 2025
 month = 4
-latitude = -37.81855964
-longitude = 144.99806817
+latitude = -38.2551
+longitude = 145.2414
 temp = temperature_monthly.query(year, month, latitude, longitude)
 print(
     f"Temperature in {year}-{month} at ({latitude}, {longitude}): {temp['temperature']} °C"
 )
 print(temp)
+
+
+# 3. When there is not grid point within the search radius, the tool will raise an exception.
+# You can catch the exception and handle it as you like.
+from global_temperature.errors import NoNearbyPointError
+
+try:
+    # Query a location which is in the ocean
+    year = 2025
+    month = 4
+    latitude = -38.1235
+    longitude = 144.9779
+    temp = temperature_monthly.query(year, month, latitude, longitude)
+    print(
+        f"Temperature in {year}-{month} at ({latitude}, {longitude}): {temp['temperature']} °C"
+    )
+except NoNearbyPointError as e:
+    print(f"No nearby point found: {e}")
